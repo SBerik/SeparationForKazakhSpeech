67cb98729a5b34fbb63cd8924f4a2b11e9f224ab
Berik S <b.smadiarov@ghalam.kz>
2024-12-04 16:48:11 +0500

    draw

--- Dataset.py
+++ Dataset.py
@@ -1,22 +1,76 @@
-import sys
-sys.path.append('../')
-
-from data_loader.AudioData import AudioReader
-import torch
-from torch.utils.data import Dataset
-
 import numpy as np
+import torchaudio
+import torch
+
+from utils.funtctional import handle_scp
+from utils.data_processing import read_wav
+from torch.utils.data import DataLoader as Loader
+
+def make_dataloader(opt):
+    # make train's dataloader
+    
+    train_dataset = Datasets(
+        opt['datasets']['train']['dataroot_mix'],
+        [opt['datasets']['train']['dataroot_targets'][0],
+         opt['datasets']['train']['dataroot_targets'][1]],
+        **opt['datasets']['audio_setting'])
+    train_dataloader = Loader(train_dataset,
+                              batch_size=opt['datasets']['dataloader_setting']['batch_size'],
+                              num_workers=opt['datasets']['dataloader_setting']['num_workers'],
+                              shuffle=opt['datasets']['dataloader_setting']['shuffle'])
+    
+    # make validation dataloader
+    
+    val_dataset = Datasets(
+        opt['datasets']['val']['dataroot_mix'],
+        [opt['datasets']['val']['dataroot_targets'][0],
+         opt['datasets']['val']['dataroot_targets'][1]],
+        **opt['datasets']['audio_setting'])
+    val_dataloader = Loader(val_dataset,
+                            batch_size=opt['datasets']['dataloader_setting']['batch_size'],
+                            num_workers=opt['datasets']['dataloader_setting']['num_workers'],
+                            shuffle=False)
+    
+    return train_dataloader, val_dataloader
 
 
-class Datasets(Dataset):
-    '''
-       Load audio data
-       mix_scp: file path of mix audio (type: str)
-       ref_scp: file path of ground truth audio (type: list[spk1,spk2])
-       chunk_size (int, optional): split audio size (default: 32000(4 s))
-       least_size (int, optional): Minimum split size (default: 16000(2 s))
-    '''
+class AudioReader(object):
+    def __init__(self, scp_path, sample_rate=8000):
+        super(AudioReader, self).__init__()
+        self.sample_rate = sample_rate
+        self.index_dict = handle_scp(scp_path)
+        self.keys = list(self.index_dict.keys())
 
+    def _load(self, key):
+        src, sr = read_wav(self.index_dict[key], return_rate=True)
+        if self.sample_rate is not None and sr != self.sample_rate:
+            raise RuntimeError('SampleRate mismatch: {:d} vs {:d}'.format(
+                sr, self.sample_rate))
+        return src
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __iter__(self):
+        for key in self.keys:
+            yield key, self._load(key)
+
+    def __getitem__(self, index):
+        if type(index) not in [int, str]:
+            raise IndexError('Unsupported index type: {}'.format(type(index)))
+        if type(index) == int:
+            num_uttrs = len(self.keys)
+            if num_uttrs < index and index < 0:
+                raise KeyError('Interger index out of range, {:d} vs {:d}'.format(
+                    index, num_uttrs))
+            index = self.keys[index]
+        if index not in self.index_dict:
+            raise KeyError("Missing utterance {}!".format(index))
+
+        return self._load(index)
+    
+
+class Datasets(torch.utils.data.Dataset):
     def __init__(self, mix_scp=None, ref_scp=None, sample_rate=8000, chunk_size=32000, least_size=16000):
         super(Datasets, self).__init__()
         self.mix_audio = AudioReader(
@@ -29,11 +83,3 @@ class Datasets(Dataset):
 
     def __getitem__(self, index):
         return self.mix_audio[index], [ref[index] for ref in self.ref_audio]
-
-
-if __name__ == "__main__":
-    dataset = Datasets("/home/likai/data1/create_scp/cv_mix.scp",
-                      ["/home/likai/data1/create_scp/cv_s1.scp", "/home/likai/data1/create_scp/cv_s2.scp"])
-    for i in dataset.mix_audio:
-        if i.shape[0] != 32000:
-            print('fail')
---
+++ utils/data_processing.py
@@ -0,0 +1,32 @@
+import torchaudio
+
+def read_wav(fname, return_rate=False):
+    '''
+         Read wavfile using Pytorch audio
+         input:
+               fname: wav file path
+               return_rate: Whether to return the sampling rate
+         output:
+                src: output tensor of size C x L 
+                     L is the number of audio frames 
+                     C is the number of channels. 
+                sr: sample rate
+    '''
+    src, sr = torchaudio.load(fname, channels_first=True)
+    if return_rate:
+        return src.squeeze(), sr
+    else:
+        return src.squeeze()
+
+
+def write_wav(fname, src, sample_rate):
+    '''
+         Write wav file
+         input:
+               fname: wav file path
+               src: frames of audio
+               sample_rate: An integer which is the sample rate of the audio
+         output:
+               None
+    '''
+    torchaudio.save(fname, src, sample_rate)
