8d98d6d690e9d9b4a26a4b80ed58c0e02541377c
Berik S <b.smadiarov@ghalam.kz>
2024-12-19 14:01:33 +0500

    upd trainer.py

--- trainer.py
+++ trainer.py
@@ -3,7 +3,6 @@ import os
 import torch
 from utils.measure_time import measure_time
 from utils.training import * 
-from utils.checkpointer import Checkpointer
 
 class Trainer:
     def __init__(self, num_epochs = 100, device='cuda', best_weights = False, checkpointing = False, 
@@ -22,101 +21,27 @@ class Trainer:
         self.resume = resume
 
     @measure_time
-    def fit(self, model, dataloaders, criterion, optimizer, metrics, writer) -> None:
+    def fit(self, model, dataloaders, criterion, optimizer, writer) -> None:
         model.to(self.device)
         min_val_loss = float('inf')
-        epoch_state = EpochState(metrics)
-
         for epoch in range(self.num_epochs):
             for phase in ['train', 'valid']:
-                
                 model.train() if phase == 'train' else model.eval()
                 dataloader = dataloaders[phase] 
+                print(phase, 'dataloader len', len(dataloader))
                 running_loss = 0.0
-                for m in metrics.keys():
-                    metrics[m].reset()
-                
                 for inputs, labels in dataloader:
-                    inputs, labels = inputs.to(self.device), labels.to(self.device)
+                    inputs = inputs.to(self.device)
+                    labels = [l.to(self.device) for l in labels]
                     with torch.set_grad_enabled(phase == 'train'):
-                        outputs = model(inputs).transpose(1, 2)
+                        outputs = model(inputs)
                         loss = criterion(outputs, labels)
                         if phase == 'train':
                             optimizer.zero_grad()
                             loss.backward()
                             optimizer.step()
-                    running_loss += loss.item() * inputs.size(0)
-                    for m in metrics.keys():
-                        metrics[m].update(outputs, labels)
-                
+                    running_loss += loss.item()
                 epoch_loss = running_loss / len(dataloader.dataset)
-                epoch_metrics = {m: metrics[m].compute().item() for m in metrics.keys()}
-                epoch_state.update_state(epoch_loss, phase, epoch_metrics)
-                p_output_log(self.num_epochs, epoch, phase, epoch_state)
-                
-                if phase == 'valid' and self.best_weights and epoch_loss < min_val_loss:
-                    min_val_loss = epoch_loss
-                    self.ckpointer.save_best_weight(model, optimizer, epoch, epoch_state)
-            
-            torch_logger(writer, epoch, epoch_state)
-            
-            if self.checkpointing and (epoch + 1) % self.checkpoint_interval == 0:
-                self.ckpointer.save_checkpoint(model, optimizer, epoch, epoch_state)
-
-
-# class Trainer(object):
-#     def __init__(self, train_dataloader, val_dataloader, Dual_RNN,  optimizer, scheduler, opt):
-#         super(Trainer).__init__()
-#         self.train_dataloader = train_dataloader
-#         self.val_dataloader = val_dataloader
-#         self.num_spks = opt['num_spks']
-#         self.cur_epoch = 0
-#         self.total_epoch = opt['train']['epoch']
-#         self.name = opt['name']
-#         if opt['train']['gpuid']:
-#             self.gpuid = opt['train']['gpuid']
-#             self.device = torch.device(
-#                 'cuda:{}'.format(opt['train']['gpuid'][0]))
-#             self.dualrnn = Dual_RNN.to(self.device)
-#         self.dualrnn = Dual_RNN.to(self.device)
-#         self.optimizer = optimizer
-#         self.clip_norm = 0
-
-#     def train(self, epoch):
-#         self.dualrnn.train()
-#         num_batchs = len(self.train_dataloader)
-#         total_loss = 0.0
-#         num_index = 1
-#         start_time = time.time()
-#         for mix, ref in self.train_dataloader:
-#             mix = mix.to(self.device)
-#             ref = [ref[i].to(self.device) for i in range(self.num_spks)]
-#             self.optimizer.zero_grad()
-
-#             if self.gpuid:
-#                 out = torch.nn.parallel.data_parallel(self.dualrnn,mix,device_ids=self.gpuid)
-#                 #out = self.dualrnn(mix)
-#             else:
-#                 out = self.dualrnn(mix)
-
-#             l = Loss(out, ref)
-#             epoch_loss = l
-#             total_loss += epoch_loss.item()
-#             epoch_loss.backward()
-
-#             if self.clip_norm:
-#                 torch.nn.utils.clip_grad_norm_(
-#                     self.dualrnn.parameters(), self.clip_norm)
-
-#             self.optimizer.step()
-#             if num_index % self.print_freq == 0:
-#                 message = '<epoch:{:d}, iter:{:d}, lr:{:.3e}, loss:{:.3f}>'.format(
-#                     epoch, num_index, self.optimizer.param_groups[0]['lr'], total_loss/num_index)
-#                 self.logger.info(message)
-#             num_index += 1
-#         end_time = time.time()
-#         total_loss = total_loss/num_index
-#         message = 'Finished *** <epoch:{:d}, iter:{:d}, lr:{:.3e}, loss:{:.3f}, Total time:{:.3f} min> '.format(
-#             epoch, num_index, self.optimizer.param_groups[0]['lr'], total_loss, (end_time-start_time)/60)
-#         self.logger.info(message)
-#         return total_loss
+                print('epoch_loss', epoch_loss)
+                # break
+                # p_output_log(self.num_epochs, epoch, epoch_loss)
