6a8eae9ad14c92d895bf44c6f4d0c8f4c0514d89
Berik S <b.smadiarov@ghalam.kz>
2024-12-20 16:31:58 +0500

    upd inference.py

--- inference.py
+++ inference.py
@@ -1,28 +1,38 @@
 import os
-import torch
-from data_loader.AudioReader import AudioReader, write_wav, read_wav
 import argparse
-from torch.nn.parallel import data_parallel
-from model.model_rnn import Dual_RNN_model
-from logger.set_logger import setup_logger
-import logging
-from config.option import parse
 import tqdm
 
+import torch 
+from models.model_rnn import Dual_RNN_model
+from utils.load_config import load_config 
+import torchaudio
+
+def get_file_name(file_path: str):
+    return os.path.splitext(os.path.basename(file_path))[0]
+
+def read_wav(fname, return_rate=False):
+    src, sr = torchaudio.load(fname, channels_first=True)
+    if return_rate:
+        return src.squeeze(), sr
+    else:
+        return src.squeeze()
+
+def write_wav(fname, src, sample_rate):
+    torchaudio.save(fname, src, sample_rate)
+
+
 class Separation():
-    def __init__(self, mix_path, yaml_path, model, gpuid):
+    def __init__(self, mixed_sample, yaml_path, weight, gpuid = 0):
         super(Separation, self).__init__()
-        self.mix = read_wav(mix_path)
-        opt = parse(yaml_path)
-        net = Dual_RNN_model(**opt['Dual_Path_RNN'])
-        dicts = torch.load(model, map_location='cpu')
-        net.load_state_dict(dicts["model_state_dict"])
-        setup_logger(opt['logger']['name'], opt['logger']['path'],
-                            screen=opt['logger']['screen'], tofile=opt['logger']['tofile'])
-        self.logger = logging.getLogger(opt['logger']['name'])
-        self.logger.info('Load checkpoint from {}, epoch {: d}'.format(model, dicts["epoch"]))
-        self.net=net
+        self.name = get_file_name (mixed_sample)
+        self.mix = read_wav(mixed_sample)
+        cfg = load_config(yaml_path)
+        self.net = Dual_RNN_model(**cfg['model'])
+        self.net.to('cpu')
+        dicts = torch.load(weight, map_location='cpu')
+        self.net.load_state_dict(dicts['model_state_dict'])
         self.gpuid = gpuid
+    
     def inference(self, file_path):
         self.net.eval()
         with torch.no_grad():
@@ -41,33 +51,26 @@ class Separation():
                 spks=[torch.squeeze(s.detach()) for s in ests]
             index=0
             for s in spks:
-                #norm
+                # Normalize
                 s = s - torch.mean(s)
-                s = s*norm/torch.max(torch.abs(s))
+                s = s * norm / torch.max(torch.abs(s))
                 index += 1
-                os.makedirs(file_path+'/spk'+str(index), exist_ok=True)
-                filename=file_path+'/spk'+str(index)+'/'+'test.wav'
-                write_wav(filename, s, 16000)
-        self.logger.info("Compute over {:d} utterances".format(len(self.mix)))
+                os.makedirs(file_path + '/spk' + str(index), exist_ok=True)
+                filename = file_path + '/spk' + str(index) + '/' + self.name + '_inferenced.flac'
+                write_wav(filename, s.unsqueeze(0), 16000)
+                print('saved in:', filename)
 
 
-def main():
-    parser=argparse.ArgumentParser()
-    parser.add_argument(
-        '-mix_scp', type=str, default='1_mix.wav', help='Path to mix scp file.')
-    parser.add_argument(
-        '-yaml', type=str, default='./config/train_rnn_opt.yml', help='Path to yaml file.')
-    parser.add_argument(
-        '-model', type=str, default='./checkpoint/Dual_Path_RNN_opt/best.pt', help="Path to model file.")
-    parser.add_argument(
-        '-gpuid', type=str, default='0', help='Enter GPU id number')
-    parser.add_argument(
-        '-save_path', type=str, default='./test', help='save result path')
-    args=parser.parse_args()
-    gpuid=[int(i) for i in args.gpuid.split(',')]
-    separation=Separation(args.mix_scp, args.yaml, args.model, [])
+def main(args):
+    gpuid = [int(i) for i in args.gpuid.split(',')]
+    separation = Separation(args.mixed_sample, args.config, args.weight, gpuid)
     separation.inference(args.save_path)
 
-
 if __name__ == "__main__":
-    main()
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-m', '--mixed_sample', type=str, default='./samples/f1_inform_1051_00_f1_inform_1082_00.flac', help='Path to mix scp file.')
+    parser.add_argument('-c', '--config', type=str, default='./configs/train_rnn.yml', help='Path to yaml file.')
+    parser.add_argument('-w', '--weight', type=str, default='./weights/Dual_Path_RNN_49_-1.2763.pt', help="Path to model file.")
+    parser.add_argument('-g', '--gpuid', type=str, default='0', help='Enter GPU id number')
+    parser.add_argument('-s', '--save_path', type=str, default='./samples', help='save result path')
+    main(parser.parse_args())
