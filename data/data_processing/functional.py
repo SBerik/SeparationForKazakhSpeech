1a3c9891154661eea9bccdbc0c0e202eb7067053
Berik S <b.smadiarov@ghalam.kz>
2024-12-23 11:34:49 +0500

    update data_processing

--- data/data_processing/functional.py
+++ data/data_processing/functional.py
@@ -1,4 +1,4 @@
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from glob import glob
 import os.path as ospth
 import re
@@ -9,12 +9,15 @@ def get_file_name(file_path: str):
 def get_audios_with_folder_name (names, all_files):
     list_folders = set(names) 
     res = []
-    for t in tqdm(glob(all_files)):
+    for t in glob(all_files):
         t = t.replace('\\', '/') 
         pattern = t.split('/')[4] 
         if pattern in list_folders:
             res.append(t)
     return res
+
+def find_files_folders (pattern, all_files): 
+    return [t.replace('\\', '/') for t in glob(all_files) if pattern in t.replace('\\', '/')]
 
 def mapper_from_flac_to_df (audio_name, base_path):
     flac_file = (base_path +  re.sub(r'.*F:/ISSAI_KSC2_unpacked/ISSAI_KSC2', '', audio_name)).replace('.flac', '.csv')
