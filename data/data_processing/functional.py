2eb812364d61682950a87a2a24dcec61c64395ed
Berik S <b.smadiarov@ghalam.kz>
2024-12-30 15:29:13 +0500

    add small changes

--- data/data_processing/functional.py
+++ data/data_processing/functional.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 from glob import glob
 import os.path as ospth
 import re
+import pandas as pd
 
 def get_file_name(file_path: str):
     return ospth.splitext(ospth.basename(file_path))[0]
@@ -17,9 +18,14 @@ def get_audios_with_folder_name (names, all_files):
     return res
 
 def find_files_folders (pattern, all_files): 
-    return [t.replace('\\', '/') for t in glob(all_files) if pattern in t.replace('\\', '/')]
+    return [t.replace('\\', '/') for t in all_files if pattern in t.replace('\\', '/')]
+
 
 def mapper_from_flac_to_df (audio_name, base_path):
     flac_file = (base_path +  re.sub(r'.*F:/ISSAI_KSC2_unpacked/ISSAI_KSC2', '', audio_name)).replace('.flac', '.csv')
     return flac_file
+
+
+def isSingleClassSample(df: pd.DataFrame) -> bool:
+    return not df['speech'].eq(0).any()
 
