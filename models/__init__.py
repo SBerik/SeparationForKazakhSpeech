d685ce587e8ce1b2ed36b467cca186a53da0e62c
Berik S <b.smadiarov@ghalam.kz>
2025-01-16 11:35:33 +0500

    rename file name

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .conv_tasnet import Conv_TasNet
-from .model_rnn import Dual_RNN_model as DualPath_RNN
+from .dualpathrnn import Dual_RNN_model as DualPath_RNN
 from .sepformer import Sepformer
 
 
