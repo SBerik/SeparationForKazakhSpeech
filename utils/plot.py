fe63788a070d15228af8f503056a1c7cb7f1b3ce
Berik S <b.smadiarov@ghalam.kz>
2025-01-27 10:43:38 +0500

    Переменовал

--- utils/plot.py
+++ utils/plot.py
@@ -2,18 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def save_graph_tb_log_metrics(first_csv_path, second_csv_path, name_ox, name_oy, loc = 'lower right', pth_save = '../pics/metrics_plot.png'):
+def save_graph_tb_log_metrics(train_csv_path, val_csv_pathn, name_ox, name_oy, loc = 'lower right', pth_save = '../pics/metrics_plot.png'):
     """
     Builds a plot of metric dependencies from two CSV files and saves the image.
     Parameters:
-    - first_csv_path (str): Path to the first CSV file (Train).
-    - second_csv_path (str): Path to the second CSV file (Validation).
+    - train_csv_path (str): Path to the first CSV file (Train).
+    - val_csv_pathn (str): Path to the second CSV file (Validation).
     - name_ox (str): Name for the X axis.
     - name_oy (str): Name for the Y axis.
     - plot (bool): If True, shows the plot.
     - saving (bool): If True, saves the plot to a file named `metrics_plot.png`.
     """
-    m_train, m_val  = pd.read_csv(first_csv_path), pd.read_csv(second_csv_path)
+    m_train, m_val  = pd.read_csv(train_csv_path), pd.read_csv(val_csv_pathn)
     epochs_train, values_train = m_train['Step'], m_train['Value']
     epochs_val, values_val = m_val['Step'], m_val['Value']
 
