14a571d4f7519c52674d7660057a847e53501469
Berik S <b.smadiarov@ghalam.kz>
2025-01-27 10:44:32 +0500

    add new functionaluty

--- utils/parser.py
+++ utils/parser.py
@@ -1,23 +1,23 @@
 import csv
 
 
-def _logger(log, stage, csv_filename = './'):
-    lines = log.split("\n")
+def _logger(logs, stage, index=0, csv_filename = './'):
+    lines = logs.split("\n")
     data = []
     
     for line in lines:
         if line.startswith("Epoch"):
             epoch = int(line.split()[1].split('/')[0]) - 1
         elif line.startswith(stage):
-            loss = float(line.split("|")[0].split(":")[1].strip())
-            data.append([1735105736, epoch, loss])
-
+            val = float(line.split("|")[index].split(":")[1].strip())
+            data.append([1735105736, epoch, val])
+            
     with open(csv_filename, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["Wall time", "Step", "Value"])  
         writer.writerows(data)
 
-    print(f"Данные успешно сохранены в файл {csv_filename}.")
+    print(f"Данные успешно сохранены в файл {csv_filename}")
 
 
 # _logger(log, 
