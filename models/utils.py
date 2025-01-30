03d733fb3e12662268facb7e8e1c1cb3b0bd3119
Berik S <b.smadiarov@ghalam.kz>
2025-01-30 15:30:23 +0500

    add normalization to the different folder

---
+++ models/utils.py
@@ -0,0 +1,91 @@
+import torch 
+
+
+class GlobalLayerNorm(torch.nn.Module):
+    '''
+       Calculate Global Layer Normalization
+       dim: (int or list or torch.Size) –
+          input shape from an expected input of size
+       eps: a value added to the denominator for numerical stability.
+       elementwise_affine: a boolean value that when set to True, 
+          this module has learnable per-element affine parameters 
+          initialized to ones (for weights) and zeros (for biases).
+    '''
+
+    def __init__(self, dim, shape, eps=1e-8, elementwise_affine=True):
+        super(GlobalLayerNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+
+        if self.elementwise_affine:
+            if shape == 3:
+                self.weight = torch.nn.Parameter(torch.ones(self.dim, 1))
+                self.bias = torch.nn.Parameter(torch.zeros(self.dim, 1))
+            if shape == 4:
+                self.weight = torch.nn.Parameter(torch.ones(self.dim, 1, 1))
+                self.bias = torch.nn.Parameter(torch.zeros(self.dim, 1, 1))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+
+    def forward(self, x):
+        # x = N x C x K x S or N x C x L
+        # N x 1 x 1
+        # cln: mean,var N x 1 x K x S
+        # gln: mean,var N x 1 x 1
+        if x.dim() == 4:
+            mean = torch.mean(x, (1, 2, 3), keepdim=True)
+            var = torch.mean((x-mean)**2, (1, 2, 3), keepdim=True)
+            if self.elementwise_affine:
+                x = self.weight*(x-mean)/torch.sqrt(var+self.eps)+self.bias
+            else:
+                x = (x-mean)/torch.sqrt(var+self.eps)
+        if x.dim() == 3:
+            mean = torch.mean(x, (1, 2), keepdim=True)
+            var = torch.mean((x-mean)**2, (1, 2), keepdim=True)
+            if self.elementwise_affine:
+                x = self.weight*(x-mean)/torch.sqrt(var+self.eps)+self.bias
+            else:
+                x = (x-mean)/torch.sqrt(var+self.eps)
+        return x
+
+
+class CumulativeLayerNorm(torch.nn.LayerNorm):
+    '''
+       Calculate Cumulative Layer Normalization
+       dim: you want to norm dim
+       elementwise_affine: learnable per-element affine parameters 
+    '''
+
+    def __init__(self, dim, elementwise_affine=True):
+        super(CumulativeLayerNorm, self).__init__(
+            dim, elementwise_affine=elementwise_affine, eps=1e-8)
+
+    def forward(self, x):
+        # x: N x C x K x S or N x C x L
+        # N x K x S x C
+        if x.dim() == 4:
+           x = x.permute(0, 2, 3, 1).contiguous()
+           # N x K x S x C == only channel norm
+           x = super().forward(x)
+           # N x C x K x S
+           x = x.permute(0, 3, 1, 2).contiguous()
+        if x.dim() == 3:
+            x = torch.transpose(x, 1, 2)
+            # N x L x C == only channel norm
+            x = super().forward(x)
+            # N x C x L
+            x = torch.transpose(x, 1, 2)
+        return x
+
+
+def select_norm(norm, dim, shape):
+    if norm == 'gln':
+        return GlobalLayerNorm(dim, shape, elementwise_affine=True)
+    if norm == 'cln':
+        return CumulativeLayerNorm(dim, elementwise_affine=True)
+    if norm == 'ln':
+        return torch.nn.GroupNorm(1, dim, eps=1e-8)
+    else:
+        return torch.nn.BatchNorm1d(dim)
