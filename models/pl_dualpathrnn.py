c7804466dd4ab87230ad072c3b9e9a6d0f7083ef
Berik S <b.smadiarov@ghalam.kz>
2025-02-04 11:58:32 +0500

    add pl training

---
+++ models/pl_dualpathrnn.py
@@ -0,0 +1,81 @@
+import os
+
+import torch
+import pytorch_lightning as pl
+from torchmetrics.audio import PermutationInvariantTraining as PIT
+from torchmetrics.functional.audio import signal_distortion_ratio as sdr
+from torchmetrics.functional.audio import scale_invariant_signal_noise_ratio as sisnr
+
+from .modules import Encoder, Decoder
+from .dualpathrnn import Dual_Path_RNN
+from utils.training import * 
+
+
+class PL_Dual_RNN_model(pl.LightningModule):
+    def __init__(self, in_channels, out_channels, hidden_channels,
+                 kernel_size=2, rnn_type='LSTM', norm='ln', dropout=0,
+                 bidirectional=False, num_layers=4, K=200, speaker_num=2,
+                 alpha=0.5, beta=0.5, lr=1e-3):
+        super(PL_Dual_RNN_model, self).__init__()
+        self.encoder = Encoder(kernel_size=kernel_size, out_channels=in_channels, bias=False)
+        self.separation = Dual_Path_RNN(in_channels, out_channels, hidden_channels,
+                                        rnn_type=rnn_type, norm=norm, dropout=dropout,
+                                        bidirectional=bidirectional, num_layers=num_layers, 
+                                        K=K, speaker_num=speaker_num)
+        self.decoder = Decoder(in_channels=in_channels, out_channels=1, 
+                               kernel_size=kernel_size, stride=kernel_size // 2, bias=False)
+        self.speaker_num = speaker_num
+        # это не параметры модели поэтому не добавляем в .yml файл
+        self.alpha = alpha
+        self.beta = beta
+        self.sisnr_criterion = PIT(sisnr)
+        self.sdr_criterion = PIT(sdr)
+
+    def forward(self, x):
+        # x: [B, L]
+        e = self.encoder(x) # output # [B, N, L]
+        s = self.separation(e) # output [spks, B, N, L]
+        out = [s[i]*e for i in range(self.speaker_num)] # [B, N, L] -> [B, L]
+        audio = [self.decoder(out[i]) for i in range(self.speaker_num)]
+        return audio
+
+    def epoch_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self(inputs)
+        labels, outputs = tensify(labels), tensify(outputs) if not isinstance(outputs, torch.Tensor) else outputs
+        sisnr_loss = -self.sisnr_criterion(outputs, labels)
+        sdr_loss = -self.sdr_criterion(outputs, labels)
+        loss = self.alpha * sisnr_loss + self.beta * sdr_loss
+        losses = {'sisnr': sisnr_loss, 'sdr': sdr_loss, 'loss': loss}
+        return losses
+
+    def log_losses(self, losses, mode='train'):
+        for loss_name, loss_value in losses.items():
+            self.log(f'{mode}_{loss_name}', loss_value, on_step=True, on_epoch=True, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        losses = self.epoch_step(batch, batch_idx)
+        self.log_losses(losses, mode="train")
+        return losses['loss']
+
+    def validation_step(self, batch, batch_idx):
+        losses = self.epoch_step(batch, batch_idx)
+        self.log_losses(losses, mode="val")
+        return losses['loss']
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", patience=3, factor=0.5)
+        if self.trained_model and os.path.exists(self.trained_model):
+            print(f"Loading pretrained model: {self.trained_model}", '\n')
+            checkpoint = torch.load(self.trained_model, map_location=self.device)
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            # scheduler.load_state_dict(checkpoint["lr_scheduler"])
+        return {
+            "optimizer": optimizer,
+            "monitor": "val_loss"  # Параметр monitor теперь на том же уровне
+        }
+
+
+if __name__ == '__main__':
+    model = model_class(**cfg['model'])
---
+++ pl_train.py
@@ -0,0 +1,47 @@
+import argparse
+from pathlib import Path
+
+import torch
+from torch.utils.tensorboard import SummaryWriter as TensorBoard
+from torchmetrics.audio import PermutationInvariantTraining as PIT
+from torchmetrics.functional.audio import signal_distortion_ratio as sdr
+from torchmetrics.functional.audio import scale_invariant_signal_noise_ratio as sisnr
+
+from utils.load_config import load_config 
+from utils.training import metadata_info, configure_optimizer
+from models import get_model
+from trainer import Trainer
+from data import DiarizationDataset
+
+torch.cuda.empty_cache()
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.set_float32_matmul_precision('medium')
+
+
+def main(hparams_file):
+    # Loading config file    
+    cfg = load_config(hparams_file)
+    # Load data 
+    datamodule = DiarizationDataset(**cfg['data']).setup(stage = 'train')
+    dataloaders = {'train': datamodule.train_dataloader(), 'valid': datamodule.val_dataloader()}
+    # Load model
+    model_class = get_model(cfg['trainer']['model_name'])
+    model = model_class(**cfg['model'])
+    # Meta-data
+    metadata_info(model)
+    # TensorBoard
+    writer = TensorBoard(f'tb_logs/{Path(hparams_file).stem}', comment = f"{cfg['trainer']['ckpt_folder']}")
+    # Optimizer
+    optimizer = configure_optimizer(cfg, model)
+    # Loss and metrics
+    loss_funcs = {name: PIT(func).to(cfg['trainer']['device']) for name, func in {"sisnr": sisnr, "sdr": sdr}.items()}
+    # Train
+    Trainer(**cfg['trainer']).fit(model, dataloaders, loss_funcs, optimizer, writer)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--hparams", type=str, default="./configs/sepformer.yml", help="hparams config file")
+    args = parser.parse_args()
+    main(args.hparams)
