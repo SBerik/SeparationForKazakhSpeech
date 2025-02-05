1686515b1b34dd8a9f094f67f0ad6caf145d0c02
Berik S <b.smadiarov@ghalam.kz>
2025-02-05 16:30:43 +0500

    add new function load_config_yml to load config file pl trainer

--- utils/load_config.py
+++ utils/load_config.py
@@ -10,3 +10,11 @@ def load_config (hparams_file):
         yaml.dump(cfg, file)
     cfg['trainer']['ckpt_folder'] = ckpt_folder
     return cfg
+
+def load_config_yml(hparams_file):
+    cfg = yaml.load(open(hparams_file), Loader=yaml.FullLoader)
+    ckpt_folder = os.path.join('./checkpoints', Path(hparams_file).stem).replace('\\', '/')
+    os.makedirs(ckpt_folder, exist_ok=True)
+    with open(os.path.join(ckpt_folder, 'hparams.yml'), 'w') as file:
+        yaml.dump(cfg, file)
+    return cfg
